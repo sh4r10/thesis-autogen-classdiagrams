7b6f7cfa48c2d8b6100708f46c3ea80ac1fa8da5
Shariq Shahbaz <iamsh4r10@gmail.com>
2024-05-03 20:00:54 +0200

    remove saving functionality from compile_stats.py

--- artefacts/pre-experiments/file-handling/compile_stats.py
+++ artefacts/pre-experiments/file-handling/compile_stats.py
@@ -1,6 +1,7 @@
 import os, pickle, argparse, statistics
 from rich import print
 from rich.pretty import pprint
+from rich.console import Console
 
 def get_f1_scores(target_dir):
     files = []
@@ -11,21 +12,17 @@ def get_f1_scores(target_dir):
     }
     for filename in os.listdir(target_dir):
         if filename.endswith(".pkl"):
-            print("Reading "+filename+"...")
             files.append(filename)
             with open(os.path.join(target_dir, filename), 'rb') as file:
                 loaded_dict = pickle.load(file)
                 scores["major"].append(loaded_dict["major"]["f1"])
                 scores["moderate"].append(loaded_dict["moderate"]["f1"])
                 scores["minor"].append(loaded_dict["minor"]["f1"])
-    print("Done.")
     return files, scores
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("compile_stats.py");
     parser.add_argument('target_dir')
-    parser.add_argument('-f', '--file', action='store')
-    parser.add_argument('-d', '--dir', action='store')
     args = parser.parse_args()
     files, scores = get_f1_scores(args.target_dir);
     stats = {
@@ -44,5 +41,3 @@ if __name__ == "__main__":
         }
     }
     pprint(stats, expand_all=True)
-    
-
