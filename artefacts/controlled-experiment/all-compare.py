870a27d3dd602f17429c793570505942014a967c
viccam <viccam@student.chalmers.se>
2024-05-18 15:23:29 +0200

    update all-compare.py

--- artefacts/controlled-experiment/all-compare.py
+++ artefacts/controlled-experiment/all-compare.py
@@ -1,8 +1,11 @@
 import os
 
-for X in range(1, 6):
-    for Y in range(1, 6):
-        command = f"python compare.py project-{X}/human-diagram.puml project-{X}/t{Y}-no-haf.puml -f t{Y}-no-haf -d project-{X}/stats-no-haf"
-        os.system(command)
-        command = f"python compare.py project-{X}/human-diagram.puml project-{X}/t{Y}-haf.puml -f t{Y}-haf -d project-{X}/stats-haf"
-        os.system(command)
+output_file = "all_commands_output.txt"
+with open(output_file, 'w') as f:
+    for X in range(1, 6):
+        for Y in range(1, 6):
+            command = f"python compare.py project-{X}/human-diagram.puml project-{X}/t{Y}-no-haf.puml -f t{Y}-no-haf -d project-{X}/stats-no-haf"
+            f.write(f"Executing command: {command}\n")
+            result = os.popen(command).read()
+            f.write(result)
+            f.write("\n\n")
